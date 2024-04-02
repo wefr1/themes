6042853e626797f29771af92b1844128b210d8d1
wefr1 <weshdzdz1@gmail.com>
2024-04-02 16:18:57 +0200

    Ajout sections pour afficher les photos dans la page d'accueil

--- nathaliemota/front-page.php
+++ nathaliemota/front-page.php
@@ -1,5 +1,23 @@
 <?php
 
-get_header();
 
+get_header();
+?>
+
+	<main>
+
+		<section id="header">
+					<?php get_template_part('template-parts/hero'); ?>
+		</section>
+
+
+		<section id="containerPhoto" class="blockCatalogue">
+					<?php get_template_part('template-parts/photo-container'); ?>
+		</section>
+
+
+    
+	</main>
+
+<?php
 get_footer();
