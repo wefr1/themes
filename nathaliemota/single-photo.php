1c2a79f6e66b39fe6f82cd22d97581f0b20a124e
wefr1 <weshdzdz1@gmail.com>
2024-04-02 16:21:19 +0200

    commenter code

--- nathaliemota/single-photo.php
+++ nathaliemota/single-photo.php
@@ -23,7 +23,11 @@ $categories = get_the_terms(get_the_ID(), 'categorie');
 $formats = get_the_terms(get_the_ID(), 'format');
 $categorie_name = $categories[0]->name;
 
-
+// Définissez les URLs des vignettes pour le post précédent et suivant
+$nextPost = get_next_post();
+$previousPost = get_previous_post();
+$previousThumbnailURL = $previousPost ? get_the_post_thumbnail_url($previousPost->ID, 'thumbnail') : '';
+$nextThumbnailURL = $nextPost ? get_the_post_thumbnail_url($nextPost->ID, 'thumbnail') : '';
 
 ?>
 
@@ -34,13 +38,13 @@ $categorie_name = $categories[0]->name;
             <div class="containerPhoto">
                      <img   src="<?php echo $photo_url; ?>" alt="<?php the_title_attribute(); ?>">
                           <div class="singlePhotoOverlay">
-                               <div  data-reference="<?php echo esc_attr($reference); ?>" data-full="<?php echo esc_url($photo_url); ?>" data-category="<?php echo esc_attr($categorie_name); ?>">
-                                     <img src="<?php echo esc_url(get_template_directory_uri()); ?>" >
+                               <div class="fullscreen-icon" data-reference="<?php echo esc_attr($reference); ?>" data-full="<?php echo esc_url($photo_url); ?>" data-category="<?php echo esc_attr($categorie_name); ?>">
+                                     <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/fullscreen.svg" alt="Icone fullscreen">
                                </div>
                           </div>
             </div>
 
-            <div class="selecteurK">
+            <div class="infoPhotos">
                 <h2><?php echo get_the_title(); ?></h2>
 
                 <div class="taxonomies">
@@ -69,6 +73,21 @@ $categorie_name = $categories[0]->name;
 
     	<div class="naviguationPhotos">
 
+      	<!-- Conteneur pour la miniature -->
+      	<div class="miniPicture" id="miniPicture">
+      	  <!-- La miniature sera chargée ici par JavaScript -->
+      	</div>
+
+      	<div class="naviguationArrow">
+      	 <?php if (!empty($previousPost)) : ?>
+      	     <img class="arrow arrow-left" src="<?php echo get_theme_file_uri() . '/assets/images/left.png'; ?>" alt="Photo précédente" data-thumbnail-url="<?php echo $previousThumbnailURL; ?>" data-target-url="<?php echo esc_url(get_permalink($previousPost->ID)); ?>">
+      	 <?php endif; ?>
+
+      	 <?php if (!empty($nextPost)) : ?>
+      	    <img class="arrow arrow-right" src="<?php echo get_theme_file_uri() . '/assets/images/right.png'; ?>" alt="Photo suivante" data-thumbnail-url="<?php echo $nextThumbnailURL; ?>" data-target-url="<?php echo esc_url(get_permalink($nextPost->ID)); ?>">
+      	 <?php endif; ?>
+      	</div>
+
       </div>
     </div>
 </section>
@@ -80,41 +99,48 @@ $categorie_name = $categories[0]->name;
 
 	<div class="PhotoSimilaire">
 
-	  <?php
+	<?php
+// Récupérer les catégories de l'article actuel
+$categories = get_the_terms(get_the_ID(), 'categorie');
 
-	  $categories = get_the_terms(get_the_ID(), 'categorie');
-	     if ($categories && !is_wp_error($categories)) {
-	           $category_ids = wp_list_pluck($categories, 'term_id');
-	               $args = array(
-	                   'post_type' => 'photos',
-	                   'posts_per_page' => 2,
-	                   'orderby' => 'rand',
-	                   'post__not_in' => array(get_the_ID()),
-	                   'tax_query' => array(
-	                            array(
-	                                'taxonomy' => 'categorie',
-	                                   'field' => 'term_id',
-	                                   'terms' => $category_ids,
-	                               ),
-	                           ),
-	                   );
+// Vérifier si des catégories existent et ne génèrent pas d'erreur
+if ($categories && !is_wp_error($categories)) {
+    // Extraire les identifiants des catégories
+    $category_ids = wp_list_pluck($categories, 'term_id');
 
-	           $compteur = 0;
-	           $related_block = new WP_Query($args);
-						     while ($related_block->have_posts()) {
-						               $related_block->the_post();
-						               $photo_url = get_the_post_thumbnail_url(null, "large");
-						               $reference = get_field('reference');
-						               $categorie_name = isset($categories[0]) ? $categories[0]->name : '';
+    // Définir les arguments pour la requête
+    $args = array(
+        'post_type' => 'photos',
+        'posts_per_page' => 2,
+        'orderby' => 'rand',
+        'post__not_in' => array(get_the_ID()),
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'categorie',
+                'field' => 'term_id',
+                'terms' => $category_ids,
+            ),
+        ),
+    );
 
-						      get_template_part('template-parts/photo_block');
-						      $compteur++;
-						     // echo $compteur;
-						      
-						     }
+    // Effectuer la requête
+    $related_block = new WP_Query($args);
 
-	     } 
-	  ?>
+    // Parcourir les résultats de la requête
+    while ($related_block->have_posts()) {
+        $related_block->the_post();
+        // Récupérer l'URL de l'image en vedette
+        $photo_url = get_the_post_thumbnail_url(null, "large");
+        // Récupérer la référence de la photo
+        $reference = get_field('reference');
+        // Récupérer le nom de la catégorie
+        $categorie_name = isset($categories[0]) ? $categories[0]->name : '';
+
+        // Inclure le modèle pour afficher le bloc de photo
+        get_template_part('template-parts/photo_block');
+    }
+}
+?>
 
 	</div>
 </section>
