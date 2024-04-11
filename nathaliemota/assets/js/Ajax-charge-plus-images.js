dd42956f27833080d63c55efd8bf07d048de22b3
wefr1 <weshdzdz1@gmail.com>
2024-04-11 22:35:50 +0200

    bouton charger plus (fini)

--- nathaliemota/assets/js/Ajax-charge-plus-images.js
+++ nathaliemota/assets/js/Ajax-charge-plus-images.js
@@ -1,6 +1,38 @@
-
 //  Chargement de plus d'images avec Ajax
 console.log("Chargement de plus d'images avec Ajax : son js est chargé");
+
+(function($) {
+    $('#plusDImage').click(function() {
+        var button = $(this),
+            data = {
+                'action': 'load_more',
+                'query': ajaxloadmore.query_vars,
+                'page': button.data('page')
+            };
+
+        $.ajax({
+            url: ajaxurl,
+            data: data,
+            type: 'POST',
+            beforeSend: function(xhr) {
+                button.text('Chargement...');
+            },
+            success: function(data) {
+                if (data === 'no_posts') {
+                    button.remove();
+                } else if(data) {
+                    button.data('page', button.data('page') + 1);
+                    $('#blockPusdImage').before($(data));
+                    button.text('Charger plus');
+                    attachEventsToImages(); 
+                } else {
+                    button.text('Plus de photos à charger');
+                }
+            }
+        });
+    });
+})(jQuery);
+
 
 
 
